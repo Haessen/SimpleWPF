9f828bb2f50f4f6f0c219e894824fd178cfaa03a
Haessen <blackandwhite56@163.com>
2021-07-09 13:38:21 +0800

    增加MVVM框架MVVMlight，添加ViewModelLocator

---
+++ Extensions/ViewModelLocator.cs
@@ -0,0 +1,48 @@
+﻿/*
+  In App.xaml:
+  <Application.Resources>
+      <vm:ViewModelLocator xmlns:vm="clr-namespace:" x:Key="Locator" />
+  </Application.Resources>
+  
+  In the View:
+  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+*/
+
+using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+using WPF.ViewModels;
+
+namespace WPF.Extensions
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        #region ViewModels
+        public static MainViewModel MainViewModel => SimpleIoc.Default.GetInstance<MainViewModel>();
+        #endregion
+
+        #region Views
+
+        #endregion
+
+        #region Constructor
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+        #endregion
+
+
+        /*-------------------------------------- Public Methods --------------------------------------*/
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+
+        /*-------------------------------------- Private Methods -------------------------------------*/
+
+    }
+}
